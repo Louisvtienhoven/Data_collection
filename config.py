6c02a329e87f44277c8db2d29320fb249c7bf3c9
Louisvtienhoven <l.j.m.vantienhoven@student.tudelft.nl>
2024-10-29 23:36:01 +0100

    setup

--- config.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# Append the heat directory to the system path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'heat'))
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-"""Running config file is necessary to import heat package. Currently, heat package is not installed in the interpreter."""
 
-import config
+
 import heat as ht
 #print(heat.__file__)  # This will show where Python is importing `heat` from
 
